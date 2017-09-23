86ad971e8cfa2d3f98fa913a07a4cfdca64ece1b
volkanb <volkanbenli92@gmail.com>
2017-09-23 14:02:31 -0400

    Added Base Project Files

---
+++ Assets/Scripts/SpriteTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTest : MonoBehaviour
+{
+    public enum SpriteStates { UNTRACKED, TRACKED }
+    public SpriteStates currentState;
+
+    void Start()
+    {
+        currentState = SpriteStates.UNTRACKED;
+    }
+
+    public void FoundSprite()
+    {
+        currentState = SpriteStates.TRACKED;
+
+    }
+
+    public void LostSprite()
+    {
+        currentState = SpriteStates.UNTRACKED;
+
+    }
+}
